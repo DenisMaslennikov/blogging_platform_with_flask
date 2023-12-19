3a675ea2a6b3ac8f821cef3b7ae4f66f32e8cbe7
Denis Maslennikov <ktotom7@yandex.ru>
2023-12-19 18:14:12 +0300

    add docs strings

--- backend/models/users.py
+++ backend/models/users.py
@@ -4,12 +4,13 @@ from .base_model import BaseModel
 
 
 class User(BaseModel):
+    """Модель пользователя"""
     __tablename__ = 'users'
 
-    username = Column(String(100), nullable=False)
-    email = Column(String(100), nullable=False)
-    password = Column(String(300), nullable=False)
-    first_name = Column(String(100), nullable=True)
-    last_name = Column(String(100), nullable=True)
-    middle_name = Column(String(100), nullable=True)
+    username = Column(String(100), nullable=False, doc='Имя пользователя')
+    email = Column(String(100), nullable=False, doc='Е-мейл')
+    password = Column(String(300), nullable=False, doc='Хеш пароля')
+    first_name = Column(String(100), nullable=True, doc='Имя')
+    last_name = Column(String(100), nullable=True, doc='Фамилия')
+    middle_name = Column(String(100), nullable=True, doc='Отчество')
 
